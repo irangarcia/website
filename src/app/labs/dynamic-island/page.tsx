1d5422ca3923f9bf840a6dea2d5cdd55dba78cdb
Iran Garcia <irangarciaj@gmail.com>
2025-04-01 16:45:24 -0300

    Add fixed height to box

--- src/app/labs/dynamic-island/page.tsx
+++ src/app/labs/dynamic-island/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div>
       <h1>Dynamic Island</h1>
       <div className="flex flex-col mt-20">
-        <div className="rounded-lg border border-slate-200 bg-white py-16 p-4">
+        <div className="rounded-lg border border-slate-200 bg-white py-16 p-4 h-[480px]">
             <DynamicIsland />
         </div>
       </div>
